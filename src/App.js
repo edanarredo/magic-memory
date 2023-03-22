bab0eccd4a35623966c877607d9ea82d2390fe22
Daniel <edanielarredondo@gmail.com>
2023-03-21 19:40:14 -0500

    Add card comparison logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false }
 ]
 
 function App() {
@@ -16,6 +16,9 @@ function App() {
   // Game state
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+
 
   // This function will get ran each game restart.
   const shuffleCards = () => {
@@ -28,7 +31,46 @@ function App() {
     setTurns(0)
   }
 
-  console.log(cards, turns);
+  // Handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
+
+  // Pass in a function and a dependency array
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+
+      // If we have a match, update card state
+      if (choiceOne.src === choiceTwo.src)  {
+
+        // Take previous card states to update state.
+        setCards(prevCards => {
+
+          // Return a new array of cards with the marked match
+          return prevCards.map(card => {
+
+            // Find the matching card and update it.
+            if (card.src === choiceOne.src)
+              return {...card, matched: true}
+            else  
+              return card;
+          })
+        })
+      }
+      else 
+        console.log("no match.");
+
+      resetTurn();
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prevTurns => prevTurns + 1)
+  }
 
   return (
     <div className="App">
@@ -37,7 +79,11 @@ function App() {
 
       <div className="card-grid">
         {cards.map(card => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard 
+            key={card.id} 
+            card={card}
+            handleChoice={handleChoice} 
+          />
         ))}
       </div>
     </div>
